2a0cffccdec7a602d62b0f9f282a0ba26b485d1c
DebanSolutions <126034986+DebanSolutions@users.noreply.github.com>
2023-03-06 04:16:52 +0100

    correcting typo error

--- test.java
+++ test.java
@@ -2,4 +2,4 @@ Jesus is the Prince of Peace
 He reigns forever more
 His Glory does not diminish
 His faithfulness is everlasting
-He is faithfuk to His name and faithful to His words
+He is faithful to His name and faithful to His words
